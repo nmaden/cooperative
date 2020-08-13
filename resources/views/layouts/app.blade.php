50bc06d282ff9435d1438e3265953f371f9d1a9c
Nurbol <nurbolmaden@gmail.com>
2020-08-13 21:39:09 +0600

    fixed changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="eQonaq – специализированная информационная система для взаимодействия мест размещения, иностранных туристов и государственных органов.">
+    <meta name="description" content="iff-company">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Специализированная информационная система  «eQonaq»</title>
+    <title>iff-company</title>
     <!-- Scripts -->
     {{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
 
